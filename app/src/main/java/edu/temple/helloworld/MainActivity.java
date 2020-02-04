8246a08f440f78678c3bc11c573b0a1bb46eee5f
Francesko <Franceskoracaku@hotmail.com>
2020-02-04 17:29:13 -0500

    Added a Button. Simply when you click it shows the message "You clicked the button".

--- app/src/main/java/edu/temple/helloworld/MainActivity.java
+++ app/src/main/java/edu/temple/helloworld/MainActivity.java
@@ -3,12 +3,22 @@ package edu.temple.helloworld;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViewById(R.id.button2).setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v){
+                Toast.makeText(getBaseContext(), "You have clicked the button", Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
